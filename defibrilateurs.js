cf5327827768152c938d9e29fe07b0981d193615
ferraina <ferraina.matthias@gmail.com>
2020-03-19 18:20:12 +0100

    code final DÉFIBRILLATEURS

--- defibrilateurs.js
+++ defibrilateurs.js
@@ -1,36 +1,40 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
-const longitudeUser = +readline().replace(",",'.');
-const latitudeUser = +readline().replace(",",'.');
+const longitudeUser = +readline().replace(",", ".");
+const latitudeUser = +readline().replace(",", ".");
 const N = parseInt(readline());
-const defibrilateurs=[];
+const defibrilateurs = [];
+let distanceMinimale = 0;
+let indexMinimal = 0;
+//création du tableau de defibrilateurs
 for (let i = 0; i < N; i++) {
     const DEFIB = readline();
-    defibrilateurs.push([...DEFIB.split(';')]);
+    defibrilateurs.push([...DEFIB.split(";")]);
 }
-    console.error(defibrilateurs)
-    
-defibrilateurs.forEach(el=>{
-    console.error(calculerDistance(el[4],el[5]))
+
+//enrichissement du tableau avec les distances calculées
+defibrilateurs.forEach(el => {
+    el.push(calculerDistance(el[4], el[5]))
+    console.error(el)
+});
+distanceMinimale = defibrilateurs[0][6]
+
+
+//determination de la distance la plus petite
+defibrilateurs.forEach((el, index) => {
+    if (el[6] < distanceMinimale) {
+        distanceMinimale = el[6];
+        indexMinimal = index
     }
-)
+});
 
-
-
-function calculerDistance(longitude,latitude){
-    longitude=+longitude.replace(",",'.');
-    latitude=+latitude.replace(",",'.');
-    console.error(longitude)
-    console.error(latitude)
-    const x = (longitude-longitudeUser)*Math.cos((latitudeUser+latitude)/2)
-    const y = (latitude-latitudeUser)
-    return Math.sqrt(Math.pow(x,2),Math.pow(y,2))*6371;
+//fonction de calcul de la distance entre 2 coordonées
+function calculerDistance(longitude, latitude) {
+    longitude = +longitude.replace(",", ".");
+    latitude = +latitude.replace(",", ".");
+    const x =
+        (longitude - longitudeUser) * Math.cos((latitudeUser + latitude) / 2);
+    const y = latitude - latitudeUser;
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) * 6371;
 }
 
-// Write an action using console.log()
-// To debug: console.error('Debug messages...');
 
-console.log('answer');
+console.log(defibrilateurs[indexMinimal][1])
